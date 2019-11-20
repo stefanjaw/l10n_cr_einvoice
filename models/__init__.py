054aab1c7f367967236c2ee0fccafcb92deaf4cb
root <root@test.c.staging-214517.internal>
2019-11-20 12:30:27 -0600

    added models to init file

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,14 @@
 # -*- coding: utf-8 -*-
 
-from . import models
+from . import accountPayment
+from . import activityCode
+from . import canton
+from . import confirmPopUp
+from . import district
+from . import emisor
+from . import invoice
+from . import neighborhood
+from . import product
+from . import provincia
+from . import receptor
+from . import tax
