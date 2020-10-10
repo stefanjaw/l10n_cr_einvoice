a19eb5a6ba6e5c3249221fb1109fe796d32b281b
jbarahona2095 <jbarahona2095@hotmail.com>
2020-10-10 23:24:25 +0000

    vista

--- models/.ipynb_checkpoints/account_journal-checkpoint.py
+++ models/.ipynb_checkpoints/account_journal-checkpoint.py
@@ -2,4 +2,4 @@ from odoo import models, fields, api
 
 class accountJournal(models.Model):
     _inherit = "account.journal"
-    electronic_doc_ids = fields.One2many('electronic.doc', 'journal_id')
+    electronic_doc_ids = fields.One2many('electronic.doc', 'sequence_id')
