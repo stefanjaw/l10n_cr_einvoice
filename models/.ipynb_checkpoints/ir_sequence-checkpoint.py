e293bfbfbd5a4181cd5c151b76b89ec20e5a0949
jbarahona2095 <jbarahona2095@hotmail.com>
2020-10-11 02:10:00 +0000

    secuencia

--- models/.ipynb_checkpoints/ir_sequence-checkpoint.py
+++ models/.ipynb_checkpoints/ir_sequence-checkpoint.py
@@ -1,12 +1,16 @@
 from odoo import models, fields, api
+import logging
+log = logging.getLogger(__name__)
 
 class irSequence(models.Model):
     _inherit = 'ir.sequence'
-    prefix_code = fields.Char(string ='codigo prefijo', compute = '_compute_prefix_code')
+    prefix_code = fields.Char(string ='codigo prefijo', compute = '_compute_prefix_code',store=True)
     
     @api.depends("prefix",)
     def _compute_prefix_code(self):
-        if len(self.prefix) == 10:
-            self.prefix_code = self.prefix[8:10]
+        for record in self:
+            if record.prefix:
+                if len(record.prefix) == 10:
+                    record.prefix_code = record.prefix[8:10]
             
             
