679f525a4712cf40b636bf46fda72b5772090c22
stefanjaw <40605674+stefanjaw@users.noreply.github.com>
2020-11-15 10:56:54 -0600

    Updated Manifest

--- __manifest__.py
+++ __manifest__.py
@@ -1,23 +1,21 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "l10n_cr_einvoice",
+    'name': "Costa Rica - Electronic Invoice",
 
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+    'summary': """Costa Rica - Electronic Invoice.""",
 
     'description': """
-        Long description of module's purpose
+        Costa Rica - Electronic Invoice.
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Avalantec",
+    'website': "http://www.avalantec.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Localization',
+    'version': '13.0.0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base','contacts','l10n_cr_zones','account_accountant','l10n_cr'],
