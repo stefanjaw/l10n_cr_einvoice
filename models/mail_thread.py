1ff0031b096f008902815cb65eb6afde6a70f10a
jbarahona2095 <jbarahona2095@hotmail.com>
2020-12-06 17:26:38 -0600

    xml

--- models/mail_thread.py
+++ models/mail_thread.py
@@ -31,10 +31,11 @@ class mailThread(models.AbstractModel):
             if msg_dict.get('message_id', ''):
                 self.env['email'].create_email(msg_dict)
                 docs = self.order_documents(msg_dict.get('attachments', ''))
+
                 mail_to = msg_dict.get('to', '')
                 email = mail_to.split('<')[1].split('>')[0]
-                fetch = self.env['fetchmail.server'].search(['user','=',email])
-                company = self.env['res.company'].search(['fecth_server','=',fetch])
+                fetch = self.env['fetchmail.server'].search([('user','=',email)])
+                company = self.env['res.company'].search([('fecth_server','=',fetch)])
                 self.env['electronic.doc'].automatic_bill_creation(docs)
 
         return RecordModel.create(data)
