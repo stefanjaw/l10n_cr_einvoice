315a03e2168fd6ed43a385ba86b949142ca578c7
jbarahona2095 <jbarahona2095@hotmail.com>
2020-12-12 15:26:59 -0600

    xml

--- models/account_move_func.py
+++ models/account_move_func.py
@@ -1043,7 +1043,7 @@ class AccountMoveFunctions(models.Model):
                                 exoneration['NombreInstitucion'] = self.fiscal_position_id.institution_name or ''
                                 exoneration['FechaEmision'] = self.fiscal_position_id.institution_name or ''
                                 exoneration['PorcentajeExoneracion'] =  percent or '0'
-                                MontoExoneracion = percent * LineaSubTotal
+                                MontoExoneracion = LineaSubTotal * ( percent / fisical.tax_src_id.amount)
                                 exoneration['MontoExoneracion'] =  MontoExoneracion
                                 inv_lines[arrayCount]['Impuesto'].update( dict({'Exoneracion': exoneration }) )
 
