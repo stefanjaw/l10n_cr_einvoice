ce9977734c614eff01bb6d9ea2d0ac144ebc38de
jbarahona2095 <jbarahona2095@hotmail.com>
2021-01-14 15:28:05 -0600

    taxed

--- models/account_move_func.py
+++ models/account_move_func.py
@@ -525,6 +525,20 @@ class AccountMoveFunctions(models.Model):
     def validar_datos_factura(self):
         
             msg = ''
+
+            if self.partner_id.state_id:                
+                if not self.partner_id.state_id.fe_code:
+                     msg += 'En el proveedor, el codigo para factura electronica de la provincia es requerida \n'
+
+                if not self.partner_id.canton_id:
+                    msg += 'En el proveedor, el canton es requerido \n'
+                    
+                if not self.partner_id.distrito_id:
+                    msg += 'En el proveedor, el distrito es requerido \n'
+
+                if not self.partner_id.street:
+                    msg += 'En el proveedor, el campo otras señas es requerido \n'
+
             if not self.fe_activity_code_id:
                 msg += 'Falta la actividad económica \n'
             elif len(self.fe_activity_code_id.code) != 6:
