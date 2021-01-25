4920ee6dd71bd84da3eb2a246be75b41cde87470
jbarahona2095 <jbarahona2095@hotmail.com>
2021-01-24 18:42:02 -0600

    Al configurar el correo inbound, detectar si ya hay uno configurado #223

--- models/fetch_email.py
+++ models/fetch_email.py
@@ -10,7 +10,7 @@ class FetchEmail(models.Model):
     @api.constrains('user')
     def _constrains_user(self):
         #Probar validacion se dispara aun que exista solo un fetch email
-        '''record = self.env['fetchmail.server'].search([('user','=',self.user)])
+        record = self.env['fetchmail.server'].search([('user','=',self.user),('id','!=',self.id)])
         if record:
            raise ValidationError('La direccion de correo {} no se puede utilizar ya que se configuro en otro Servidores de correo entrante'.format(self.user))'''
 
