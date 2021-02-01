b3d26e9d272ccdd9bb83b66a7219f9c46c0be277
jbarahona2095 <jbarahona2095@hotmail.com>
2021-01-31 19:55:40 -0600

    default fe_in_invoice

--- models/account_move_func.py
+++ models/account_move_func.py
@@ -32,14 +32,12 @@ class AccountMoveFunctions(models.Model):
     @api.model
     def default_fe_in_invoice_type(self):
         journal = super(AccountMoveFunctions, self)._get_default_journal()
-        log.info('---------------{}'.format(journal.sequence_id.prefix))
         if len(journal.sequence_id.prefix) == 10 :
                 if journal.sequence_id.prefix[8:10] == '08':
                    return 'FEC'
                 elif journal.sequence_id.prefix[8:10] == '09':
                     return 'FEX'
                 elif journal.sequence_id.prefix[8:10] == '01':
-                    log.info('---------------FE-------')
                     return 'FE'
                 elif journal.sequence_id.prefix[8:10] == '02':
                     return 'ND'
