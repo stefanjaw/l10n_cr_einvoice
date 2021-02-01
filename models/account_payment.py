6f2590eaa22484d5708b0cde97efb068f2ca123b
stefanjaw <40605674+stefanjaw@users.noreply.github.com>
2021-01-31 18:36:25 -0600

    Traducida la condición de la venta a Español

--- models/account_payment.py
+++ models/account_payment.py
@@ -5,14 +5,17 @@ from odoo import models, fields, api
 class AccountPayment(models.Model):
     _inherit = "account.payment.term"
     fe_condition_sale = fields.Selection([
-        ('01', "Immediate Payment"),
-        ('02', "Credit"),
-        ('03', "Consignment"),
-        ('04', "Separated"),
-        ('05', "Lease with purchase option"),
-        ('06', "Leasing in financial function"),
-        ('99', "Others"),
-    ], string="Sale Type", track_visibility='onchange')
+        ('01', "Contado"),
+        ('02', "Crédito"),
+        ('03', "Consignación"),
+        ('04', "Apartado"),
+        ('05', "Arrendamiento con opción de compra"),
+        ('06', "Arrendamiento en función financiera"),
+        ('07', "Cobro a favor de un tercero"),
+        ('08', "Servicios prestados al Estado a crédito"),
+        ('09', "Pago del servicios prestado al Estado"),
+        ('99', "Otros (se debe indicar la condición de la venta)"),
+    ], string="Condición de la Venta", track_visibility='onchange')
     payment_term_hacienda = fields.Char(string="Plazo Credito Hacienda",size=10)
     account_invoice_refund_ids = fields.One2many(
         string="account_invoice_refund_ids",
