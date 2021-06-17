0b9e2fdd4630823cedcdb1d3d315adfbb6d7fd45
stefanjaw <40605674+stefanjaw@users.noreply.github.com>
2021-06-17 09:25:46 -0600

    Update mail_thread.py

--- models/mail_thread.py
+++ models/mail_thread.py
@@ -38,7 +38,7 @@ class mailThread(models.AbstractModel):
                 mail_to = mail_to.replace(" ", "").split(",")
                 log.info("=====mail_to===={}".format(mail_to))
                 #fetch = self.env['fetchmail.server'].search([('user','=',mail_to)])
-                fetch = self.env['fetchmail.server'].search([('user','in', mail_to )])
+                fetch = self.env['fetchmail.server'].search([('user','in', mail_to )], limit=1)
                 company = self.env['res.company'].search([('fecth_server','=',fetch.id)])
                 self.env['email'].create_email(msg_dict,company)
                 docs = self.order_documents(msg_dict.get('attachments', ''))
