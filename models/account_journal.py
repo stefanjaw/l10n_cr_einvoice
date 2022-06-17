bc09b9a2e92d1de43000ef27a95c722f13a6f386
Stefan J <stefanj@avalantec.com>
2022-06-17 17:27:00 +0000

    Updated

--- models/account_journal.py
+++ models/account_journal.py
@@ -11,9 +11,9 @@ class AccountJournal(models.Model):
     
     _logging.info("DEB __________ account.journal" )
 
-    sequence_fe = fields.Many2one('ir.sequence', string='Secuencia Factura Electrónica')
-    sequence_nd = fields.Many2one('ir.sequence', string='Secuencia Notas Debito')
+    sequence_fe = fields.Many2one('ir.sequence', string='Secuencia Factura Electrónica', help='Default Prefix: 0010000101')
+    sequence_nd = fields.Many2one('ir.sequence', string='Secuencia Notas Debito', help='Default Prefix: 0010000102')
     #sequence_nc = fields.Many2one('ir.sequence', string='Secuencia Notas Crédito')
-    sequence_te = fields.Many2one('ir.sequence', string='Secuencia Tiquete Electrónico')
-    sequence_fec = fields.Many2one('ir.sequence', string='Secuencia Factura Electrónica de Compra')
-    sequence_fee = fields.Many2one('ir.sequence', string='Secuencia Factura Electrónica Exportación')
+    sequence_te = fields.Many2one('ir.sequence', string='Secuencia Tiquete Electrónico', help='Default Prefix: 0010000104')
+    sequence_fec = fields.Many2one('ir.sequence', string='Secuencia Factura Electrónica de Compra', help='Default Prefix: 0010000108')
+    sequence_fee = fields.Many2one('ir.sequence', string='Secuencia Factura Electrónica Exportación', help='Default Prefix: 0010000109')
