468ec184788d86d960b9b180c6544b22170a0b26
root <root@instance-staging1.c.staging-214517.internal>
2022-09-07 01:19:39 +0000

    updated

--- models/account_fiscal_position_exoneraciones_cr.py
+++ models/account_fiscal_position_exoneraciones_cr.py
@@ -3,7 +3,9 @@
 from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 
+import datetime, pytz
 import requests
+
 import logging
 _logging = _logger = logging.getLogger(__name__)
 
@@ -22,7 +24,7 @@ class AccountFiscalPositionExoneracionesCR(models.Model):
         ('99', 'Otros'),
     ], string="Tipo de Documento")
 
-    document_number = fields.Char( string="Número de Documento" )
+    name = fields.Char( string="Número de Documento" )
     partner_id = fields.Many2one( 'res.partner', string="Empresa" )
     partner_vat = fields.Char( related='partner_id.vat', string="Identification" )
     cfia_project_code = fields.Char( string="Código Proyecto CFIA" )
@@ -31,14 +33,59 @@ class AccountFiscalPositionExoneracionesCR(models.Model):
     issued_date = fields.Datetime( string="Fecha de la Emisión" )
     expiration_date = fields.Datetime( string="Fecha de Expiración" )
     has_cabys = fields.Boolean( string="Posee Cabys" )
+    autorizacion = fields.Char( string="Autorización" )
     
-    api.onchange('document_number')
+    @api.onchange('name')
     def get_autorizacion(self):
+        _logging.info(f"DEF37 self: {self}")
         for record in self:
-            url1 = f"https://api.hacienda.go.cr/fe/ex?autorizacion={record.document_number}"
-            response = requests.get(url1, timeout=5)
-            _logging.info(f"DEF40 response: {response.json()}")
-        _logging.info(f"DEF35 autoriacion: {autorizacion}")
-        STOP35
+            _logging.info(f"DEF39 record: {record}")
+            _logging.info(f"DEF40 name: {record.name}")
+            if record.name:
+                _logging.info(f"DEF42 document_number: {record.name}")
+                url1 = f"https://api.hacienda.go.cr/fe/ex?autorizacion={record.name}"
+                _logging.info(f"DEF44 url1: {url1}")
+                response = requests.get(url1, timeout=5)
+                
+                try:
+                    response_json = response.json()
+                except:
+                    response_json = False
+                    continue
+
+                partner_id = self.env['res.partner'].search([('vat','=', response_json.get('identificacion'))])
+                if len(partner_id) > 1:
+                    partner_id = self.env['res.partner'].search([
+                        ('vat','=', response_json.get('identificacion') ),
+                        ('child_ids', '!=', False),
+                    ])
+
+                if len( partner_id ) == 0:
+                    msg = f"No Encontrada la Identificación Física o Jurídica: { response_json.get('identificacion') }"
+                    raise ValidationError(msg)
+
+                issued_date = datetime.datetime.fromisoformat( response_json.get('fechaEmision') + "+00:00" )#.astimezone( pytz.timezone('America/Costa_Rica') )
+                _logging.info(f"DEF67 issued_date: {issued_date}")
+                expiration_date = datetime.datetime.fromisoformat( response_json.get('fechaVencimiento') )#.astimezone( pytz.timezone('America/Costa_Rica') )
+                try:
+                    codigo = response_json.get('tipoDocumento').get('codigo')
+                except:
+                    codigo = False
+
+                _logging.info(f"DEF64 CFIA: {response_json.get('codigoProyectoCFIA') }")
+
+                record.write({
+                    'partner_id': partner_id[0].id or False,
+                    'fiscal_position_type': codigo,
+                    'cfia_project_code': str(response_json.get('codigoProyectoCFIA') ) or False,
+                    'issued_date': str(issued_date),
+                    'institution_name': response_json.get('nombreInstitucion') or False,
+                    'expiration_date': str(expiration_date),
+                    'exoneration_percentage': response_json.get('porcentajeExoneracion') or 0,
+                    'has_cabys': response_json.get('poseeCabys') or False,
+
+                })
+
+                _logging.info(f"DEF62 FIN =====")
 
 
