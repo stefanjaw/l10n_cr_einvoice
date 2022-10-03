923473b90d8ef1f005fb5553de821bf65ff3e264
root <root@instance-staging1.c.staging-214517.internal>
2022-10-03 16:41:31 +0000

    Updated

--- models/account_fiscal_position.py
+++ models/account_fiscal_position.py
@@ -6,6 +6,7 @@ from odoo.exceptions import ValidationError
 class AccountFiscalPosition(models.Model):
     _inherit = "account.fiscal.position"
 
+    '''
     fiscal_position_type = fields.Selection([
         ('01', 'Compras autorizadas'),
         ('02', 'Ventas exentas a diplomáticos'),
@@ -19,10 +20,10 @@ class AccountFiscalPosition(models.Model):
     document_number = fields.Char(string="Número de Documento")
     institution_name = fields.Char(string="Nombre de la Institución")
     issued_date = fields.Date(string="Fecha de la Emisión")
-    
 
     @api.constrains('tax_ids')
     def _constrains_tax_ids(self):
         for record in self:
             if len(self.tax_ids) == 0:
                 raise ValidationError('En Mapeo de impuestos debe de existir al menos una linea')
+    '''
