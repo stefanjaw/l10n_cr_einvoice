95e98587ed1222628b85fcd9b0a47fdb4d403492
Stefan J <stefanj@avalantec.com>
2025-09-08 17:01:03 +0000

    Updated

--- models/mail_thread.py
+++ models/mail_thread.py
@@ -17,10 +17,9 @@ class mailThread(models.AbstractModel):
 
     @api.model
     def message_new(self, msg_dict, custom_values=None):
-        _logger.info(f"DEF20 message_new self: {self}" )
-        "OK UC01"
+        _logger.info(f"    ==== message_new self: {self} | {self._name}" )
         "(['to', 'message_type', 'date', 'email_from', 'message_id', 'attachments', 'cc', 'author_id', 'from', 'subject', 'body'])"
-        _logger.info(f"DEF23 self._context: {self._context}" )
+        
         data = {}
         if isinstance(custom_values, dict):
             data = custom_values.copy()
@@ -28,39 +27,47 @@ class mailThread(models.AbstractModel):
         RecordModel = self.env[model]
         fields = RecordModel.fields_get()
         name_field = RecordModel._rec_name or 'name'
-        fetchmail_server_int = self._context.get('default_fetchmail_server_id')
-        company = self.env['res.company'].sudo().search([('fecth_server.id','=', fetchmail_server_int)])
-        _logger.info(f"DEF32 company_id: {company}\n")
         
         if name_field in fields and not data.get('name'):
             data[name_field] = msg_dict.get('subject', '')
-
-        if data.get('company_id'):
-            data['company_id'] = company.id
-        _logger.info(f"DEF40 data: {data}")
+        
         if msg_dict:
             if msg_dict.get('message_id', ''):
                 mail_to = msg_dict.get('to', '')
                 if mail_to:
                     mail_to = mail_to.lower()
                 
-                _logger.info("    =====mail_to==== {}".format(mail_to))
+                _logger.info("        =====mail_to==== {}".format(mail_to))
 
-                company_id = False
+                company_id = []
                 company_ids = self.env['res.company'].search([])
+                email_found = False
                 for company_id in company_ids:
-                    if company_id.fe_email in mail_to:
-                        _logger.info(f"    Found company_id: {company_id} for: { company_id.fe_email }")
+                    if email_found == True:
                         break
+                    elif company_id.fe_email:
+                        company_fe_mails = company_id.fe_email.replace(" ", "").replace(";", ",").split(",")
+                        for company_fe_mail in company_fe_mails:
+                            if company_fe_mail in mail_to:
+                                email_found = True
+                                break
+                        if email_found == True:
+                            break
+                        else:
+                            company_id = []
+                    else:
+                        company_id = []
                 
-                mail_to_lst = mail_to.split(",")
-                _logger.info(f"DEF46 company: {company_id}")
-                self.env['email'].create_email(msg_dict,company_id)
-                docs = self.order_documents(msg_dict.get('attachments', ''), company_id)
-                
-                self.env['electronic.doc'].automatic_bill_creation(docs,company_id)
+                if len( company_id ) > 0:
+                    self.env['email'].create_email(msg_dict,company_id)
+                    docs = self.order_documents(msg_dict.get('attachments', ''), company_id)
+                    self.env['electronic.doc'].automatic_bill_creation(docs,company_id)
+                else:
+                    msg1 = f"Not found company_id for destination email : {mail_to} ====="
+                    _logger.info( msg1 )
         
-        return RecordModel.create(data)
+        record_model_id = RecordModel.create(data)
+        return record_model_id
 
     def order_documents(self, attachments, company_id=False):
         _logger.info(f"DEF65 order_documents self: {self}")
@@ -68,11 +75,11 @@ class mailThread(models.AbstractModel):
         bills = []
         acceptance = []
         others = []
-        _logger.info(f"DEF70 company_id: {company_id}")
+        _logger.info(f"DEF79 company_id: {company_id}")
         if company_id:
             pass
         else:
-            msg1 = f"74 No company_id found: {company_id} for self: {self}"
+            msg1 = f"83 No company_id found: {company_id} for self: {self}"
             raise ValidationError( msg1 )
         
         for item in attachments:
@@ -86,7 +93,7 @@ class mailThread(models.AbstractModel):
                 try:
                     dic = self.env['electronic.doc'].convert_xml_to_other( doc,type_dest = type_dest, company_id=company_id )
                 except Exception as e:
-                    _logger.info(f"    88 Error: Can't convert XML {item.fname} to:{type_dest} - Content: \n{item.content}\nError: {e}")
+                    _logger.info(f"    97 Error: Can't convert XML {item.fname} to:{type_dest} - Content: \n{item.content}\nError: {e}")
                     continue
                 _logger.info(f"    ==== dic converted ====\ndic: {dic}")
                 doc_type = electronic_doc.get_doc_type(dic)
