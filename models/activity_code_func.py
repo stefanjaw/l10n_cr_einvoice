ab2ef2f30e4332ff643b2294e38d66718e4d0a3d
Stefan J <stefanj@avalantec.com>
2025-12-26 16:32:21 +0000

    Updated

--- models/activity_code_func.py
+++ models/activity_code_func.py
@@ -2,10 +2,18 @@
 
 from odoo import models, fields, api
 
+import logging
+_logging = _logger = logging.getLogger(__name__)
+
 class ActivityCodeFunctions(models.Model):
     _inherit = "activity.code"
     
     @api.depends()
     def compute_name(self):
         for s in self:
-            s.name = s.code+" "+s.description
+            if s.code == False:
+                s.code = ""
+            if s.description == False:
+                s.description = ""
+            
+            s.name = s.code + " " + s.description
